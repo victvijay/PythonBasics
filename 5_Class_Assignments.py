4258a102856424cb150bcfa6eb33c9592fc9d8a2
CSVijayabasker <csvijayabasker@gmail.com>
2023-11-01 00:03:51 +0530

    Add files via upload

---
+++ 5_Class_Assignments.py
@@ -0,0 +1,9 @@
+from Function_Assignment_1 import SubfieldsInAI,OddEven,ElegibilityForMarriage, FindPercent,Triangle
+SubfieldsInAI.Subfields()
+OddEven.OE()
+ElegibilityForMarriage.Elegible()
+FindPercent.percentage()
+Triangle.formula()
+
+
+
